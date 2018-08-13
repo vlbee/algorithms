7790d70078db430bb0a0f34ca222ae3431d80467
vlbee <32312712+vlbee@users.noreply.github.com>
2018-08-13 10:41:07 +0100

    typo fix

--- insertionSort/insertionSort.test.js
+++ insertionSort/insertionSort.test.js
@@ -7,7 +7,7 @@ test('insertionSort returns correctly', (t) => {
   if (actual instanceof Array) {
     t.pass('array returned')
   }
-  t.deepEqual(actual, expected, 'quickSort([1, 4, 2, 0, 3]) returned [0, 1, 2, 3, 4]')
+  t.deepEqual(actual, expected, 'insertionSort([1, 4, 2, 0, 3]) returned [0, 1, 2, 3, 4]')
 
   actual = insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
   expected = [1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]

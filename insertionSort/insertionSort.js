87d59d2a94587f118db28dfe148c6bae0e8fb72a
vlbee <32312712+vlbee@users.noreply.github.com>
2018-08-13 10:35:48 +0100

    add comments and improve tests for insertionSort

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -1,12 +1,22 @@
 function insertionSort(array) {
-
+  // console.log('start', array)
+  // loops through the array 
   for (let i = 1; i < array.length; i++) {
+    // sets the second item in the array as a constant value to be compared to within second loop
     const val = array[i]
-
+    // console.log('i:', { val })
+    // console.log('array', array)
+    // for each const value, loops through array a second time, checking through all items the the LEFT of the current value's position.
     for (let j = i - 1; j >= 0; j--) {
+      // if an item to the LEFT of the current value is greater than the current value...
+      // console.log('j val:', array[j])
       if (val < array[j]) {
+        // ... then it gets swapped with the current value postion to be the RIGHT side of the current value.
         array.splice(j, 2, array[j + 1], array[j])
+        // console.log('splice', array)
+        // as the j loop decreases to the start of the array, the current value always in the index position one greater than j, and gets swapped down all the way to index 0 if needed. 
       }
+      // do nothing if item to the LEFT of the current value is smaller than the current value...
     }
 
   }
--- insertionSort/insertionSort.test.js
+++ insertionSort/insertionSort.test.js
@@ -2,17 +2,20 @@ const test = require('tape');
 const insertionSort = require('./insertionSort')
 
 test('insertionSort returns correctly', (t) => {
-  let actual = insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
-  let expected = [1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]
+  let actual = insertionSort([1, 4, 2, 0, 3])
+  let expected = [0, 1, 2, 3, 4]
   if (actual instanceof Array) {
     t.pass('array returned')
   }
+  t.deepEqual(actual, expected, 'quickSort([1, 4, 2, 0, 3]) returned [0, 1, 2, 3, 4]')
+
+  actual = insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
+  expected = [1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]
   t.deepEqual(actual, expected, 'insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]) returned [1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643]')
 
   actual = insertionSort([1, 4, 2, -8, 345, 3, 43, 32, 5643, 63, 0, -2.5, 2, 55, 1, 0])
   expected = [-8, -2.5, 0, 0, 1, 1, 2, 2, 3, 4, 32, 43, 55, 63, 345, 5643]
   t.deepEqual(actual, expected, 'insertionSort([1, 4, 2, -8, 345, -1, 43, 32, 5643, 63, 0, 43, 2, 55, 1, 0, 92]) returned [ -8, -2.5, 0, 0, 1, 1, 2, 2, 3, 4, 32, 43, 55, 63, 345, 5643 ]')
-
 
   t.end();
 });
